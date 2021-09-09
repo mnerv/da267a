fe7e98fd4619d450d3e194cca077c0d26863888b
KTheXIII <pratchaya.k.git@gmail.com>
2021-09-09 08:49:01 +0200

    Lab01 - New random

--- Lab01/src/random.c
+++ Lab01/src/random.c
@@ -3,8 +3,7 @@
 #include "esp_system.h"
 
 uint32_t rand_range(uint32_t min, uint32_t max) {
-	uint32_t value = esp_random() % max;
-	return value > min ? value : min;
+	return (esp_random() % (max - min)) + min;
 }
 
 uint32_t rand_max(uint32_t max) {
