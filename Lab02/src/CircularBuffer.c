30fed21ae8344d55c8317ab30bc8c333c1994402
KTheXIII <pratchaya.k.git@gmail.com>
2021-09-18 01:13:09 +0200

    RemoveValue bug which don't remove anything

--- Lab02/src/CircularBuffer.c
+++ Lab02/src/CircularBuffer.c
@@ -2,7 +2,7 @@
  * @file   CircularBuffer.c
  * @author Pratchaya Khansomboon (pratchaya.k.git@gmail.com)
  * @brief  Circular Buffer implemenation with int32_t.
- *         This only stores up to BUFFER_SIZE - 1.
+ *		   This only stores up to BUFFER_SIZE - 1.
  * @date   2021-09-15
  *
  * @copyright Copyright (c) 2021
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 void InitCircularBuffer(CircularBuffer* buffer, int32_t* data, int32_t maxLength) {
-	buffer->data      = data;
+	buffer->data	  = data;
 	buffer->maxLength = maxLength;
 	buffer->head = 0;
 	buffer->tail = 0;
@@ -35,14 +35,13 @@ int32_t CircularBufferRemoveValue(CircularBuffer* buffer, int32_t value) {
 	int32_t count = 0;
 	while(it != buffer->tail) {
 		if (buffer->data[it] == value) {
-			count++;
 			for (int32_t i = it; i != buffer->tail; i = (i + 1) % buffer->maxLength)
-				buffer->data[i] = buffer->data[i];
+				buffer->data[i] = buffer->data[(i + 1) % buffer->maxLength];
+			it = buffer->head;
+			buffer->tail = buffer->tail > 0 ? buffer->tail - 1 : buffer->maxLength - 1;
 		}
 		it = (it + 1) % buffer->maxLength;
 	}
-	buffer->tail = (buffer->head + (count - 1)) % buffer->maxLength;
-
 	return count == 0 ? INT32_MIN : value;
 }
 int32_t CircularBufferRemoveHead(CircularBuffer* buffer) {
@@ -53,7 +52,7 @@ int32_t CircularBufferRemoveHead(CircularBuffer* buffer) {
 	return re;
 }
 void CircularBufferPrint(CircularBuffer* buffer) {
-	for	(int32_t i = buffer->head; i != buffer->tail; i = (i + 1) % buffer->maxLength) {
+	for (int32_t i = buffer->head; i != buffer->tail; i = (i + 1) % buffer->maxLength) {
 		printf("| %d ", buffer->data[i]);
 	}
 	printf("|\n");
